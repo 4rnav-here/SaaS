b474d49c57d1f49d2e1d25d34ed26d8714806d4d
Arnavongit <arnavtrivediofficial@gmail.com>
2025-09-19 03:28:54 +0530

    Added companion card

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,47 @@
 import React from 'react'
-import {Button} from "@/components/ui/button";
+import CompanionCard from '@/components/ui/CompanionCard';
+import CompanionsList from '@/components/ui/CompanionsList';
+import CTA from '@/components/ui/CTA';
 
 const Page = () => {
   return (
-    <div>
-        <h1 className={"text-2xl underline"}>Starting the new project</h1>
-        <Button >Testing shadcn components </Button>
-    </div>
+    <main>
+      <h1 className="text-2xl underline">Popular Companions</h1>
+      
+      <section className='home-section'>
+      <CompanionCard 
+        id="123"
+        name="Dumbledore The Maths Guy"
+        topic="Calculus"
+        subject="Mathematics"
+        duration={45}
+        color="#ffff66"
+
+        />
+      <CompanionCard 
+        id="456"
+        name="Snape The Crazy Science Nerd"
+        topic="Potions and Antibiotics"
+        subject="Science"
+        duration={30}
+        color="#00cc00"
+      />
+      <CompanionCard
+        id="123"
+        name="Potter The History Teller"
+        topic="History"
+        subject="Social Science"
+        duration={30}
+        color="#3399ff"
+      />
+      </section>
+
+      <section className='home-section'>
+        <CompanionsList />
+        <CTA />
+      </section>
+
+    </main>
 
   )
 }
